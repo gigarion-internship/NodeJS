5540cc3cd9102d41a32262a35a009808e906b91b
DevKhiet <154591533+DevKhiet@users.noreply.github.com>
2024-01-15 15:39:52 +0700

    Update note.route.ts

--- TypeScript_Prisma/src/routes/note.route.ts
+++ TypeScript_Prisma/src/routes/note.route.ts
@@ -7,8 +7,6 @@ const router = express.Router();
 router.get('/', getAll);
 router.get('/:id', getById);
 router.post('/', parseSchema(CreateNoteSchema), create);
-
-// Apply the parseSchema middleware to validate the request body
 router.put('/:id', parseSchema(CreateNoteSchema), updateById);
 router.delete('/:id', deleteById);
 
